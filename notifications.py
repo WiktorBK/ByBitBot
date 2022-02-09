f4c5f566f0fb5b9ce9a6fa2e4471a2e6ca990fd1
WiktorBK <82653550+WiktorBK@users.noreply.github.com>
2022-02-09 15:42:54 +0100

    Update notifications.py

--- notifications.py
+++ notifications.py
@@ -1,94 +1,79 @@
-import secrets
+import secretstest
 from pushbullet import PushBullet
-from datetime import datetime
-from twilio.rest import Client
 import smtplib
 from email.message import EmailMessage
+from datetime import datetime, date
 
-
-
-def get_excel_file():
-    with open(r'C:\Users\admin\Desktop\Trades\TradeHistory.xlsx', 'rb') as f:
-        file_data = f.read()
-        file_name = "TradeHistory.xlsx"
-    return file_data, file_name
-
-# Email
-def send_email(subject, body, addattachment):
-
+def send_email(subject, body):
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.ehlo()
         smtp.starttls()
         smtp.ehlo()
-
         # Create a message
         msg = EmailMessage()
         msg['Subject'] = subject
-        msg['From'] = secrets.user
-        msg['To'] = secrets.user
+        msg['From'] = secretstest.user
+        msg['To'] = secretstest.user
         msg.set_content(body)
-
-        # add attachment if necessary
-        if addattachment == True:
-            msg.add_attachment(get_excel_file()[0], maintype='application', subtype='octet-stream', filename = get_excel_file()[1])
-
-
-        smtp.login(secrets.user, secrets.password)
+        smtp.login(secretstest.user, secretstest.password)
         smtp.send_message(msg)
 
-
-#Twilio
-tw_client = Client(secrets.account_sid, secrets.auth_token)
-def twilio_send_message():
-    msg = tw_client.messages.create(
-        body="twilio message",
-        from_= secrets.twilio_number,
-        to= secrets.phone_number
-    )
-
-
 # Push Bullet
-pb = PushBullet(secrets.accesstoken)
+pb = PushBullet(secretstest.accesstoken)
 now = datetime.now()
 hour = now.strftime("%H:%M:%S")
 
-# notification - trade close
-def ntc(entry_price, price_now, hour):
-    pb.push_note("Trade Info", 
-    f"{hour}\n"
-    "position closed\n"
-    f"entry price: {entry_price}\n"
-    f"price now: {price_now}"
-    )
-
-    send_email("Trade Info", "Position closed\ncheck out results in excel", addattachment = True)
-# notification - trade open
-def nto(entry_price, side, hour):
-    pb.push_note("Trade Info", 
-    f"{hour}\n"
-    "postition opened\n"
-    f"entry price: {entry_price}\n"
-    f"side: {side}"
-    )
-    send_email("Trade Info", "Position Opened\nMore details in Pushbullet app", addattachment = False)
-# notification - session
-def ns(type, hour):
+##########
+def ntc(entry_price, price_now, pnl, hour):  # notification - trade close
+    pb.push_note("Trade Info",
+                 f"{hour}\n"
+                 "position closed\n"
+                 f"entry price: {entry_price}\n"
+                 f"price now: {price_now}\n"
+                 f"pnl: {pnl}"
+                 )
+    send_email(
+        "Trade Info", "Position closed\ncheck out results in Pushbullet app")
+def nto(entry_price, side, hour): # notification - trade open
+    pb.push_note("Trade Info",
+                 f"{hour}\n"
+                 "postition opened\n"
+                 f"entry price: {entry_price}\n"
+                 f"side: {side}"
+                 )
+    send_email(
+        "Trade Info", "Position Opened\nMore details in Pushbullet app")
+def ns(type, hour): # notification - session
     pb.push_note("ByBit Bot",
-    f"Session {type} at {hour}"
-    )
+                 f"Session {type} at {hour}"
+                 )
     if type == "Closed":
-        send_email("ByBit Bot", "Application closed\nMore details in Pushbullet app", addattachment = False)
-#notification - lost connection
-def nlc(hour):
+        send_email(
+            "ByBit Bot", "Application closed\nMore details in Pushbullet app")
+def nlc(hour): # notification - lost connection
     pb.push_note("ByBit Bot",
-    f"{hour}\n"
-    f"Play sie zesrał"
-    )
-    send_email("ByBit Bot", "Lost Connection\nMore details in Pushbullet app", addattachment = False)
-# notifcation - reconnecting
-def nrc(hour):
+                 f"{hour}\n"
+                 f"Play sie zesrał"
+                 )
+    send_email(
+        "ByBit Bot", "Lost Connection\nMore details in Pushbullet app")
+def nrc(hour): # notifcation - reconnecting
     pb.push_note("ByBit Bot",
-    f"{hour}\n"
-    "Reconnected"
-    )
-
+                 f"{hour}\n"
+                 "Reconnected"
+                 )
+def nupd(msg, hour): # notification - update
+    pb.push_note("Bybit Bot",
+                 f"{hour}\n"
+                 f"{msg}"
+                 )
+def ncon(msg, hour, side, entry): # notification - trade continuation
+    pb.push_note("Trade Info",
+                 f"{hour}\n"
+                 f"{msg}\n"
+                 f"side: {side}\n"
+                 f"entry price: {entry}"
+                 )
+def nres(hour):
+    pb.push_note("Bot Info", f"{hour}\nRestarted")
+##########
