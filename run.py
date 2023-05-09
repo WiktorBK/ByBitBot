71194910b1a00d276b599a7fdc1ad2769f2498e2
wiktor bozek <wiktorsebbozek@gmail.com>
2023-05-10 00:05:53 +0200

    Create run file with scheduling

---
+++ run.py
@@ -0,0 +1,14 @@
+# from apscheduler.schedulers.background import BlockingScheduler
+# from jobs.jobs import Test
+
+# scheduler = BlockingScheduler(daemon=True)
+
+# scheduler.add_job(Test.run, 'interval', seconds=3)
+
+# scheduler.start()
+
+
+
+# if in trade run function to check if still in trade
+
+# if not in trade then check all the signals and wait for entry
