0e6ab0f2d28c3c289d7eaf1f6f9074ddbada8ba2
wiktor bozek <wiktorsebbozek@gmail.com>
2023-05-11 00:18:33 +0200

    Handle placing orders with a function

--- bybit.py
+++ bybit.py
@@ -37,8 +37,37 @@ class Bybit():
 
         data = self.get_data()
 
+        try: 
+            data = pd.DataFrame(data[:-1]) # cut last data candle
+            data = data.iloc[:, 5:11] # reduce column amount
+            data.columns = ['Time', 'Open', 'High', 'Low', 'Close', 'Volume']  # specify columns
+            data = data.set_index('Time') 
+            data.index = pd.to_datetime(data.index, unit='s')
+            data = data.astype(float)
+        except:
+            print(f"An error occured while formatting data: {traceback.format_exc()}")
+        return data
 
-print(bybit.get_data(calendar.timegm(datetime.datetime.utcnow().utctimetuple()) - 3600))
+    def place_order(self, side):
+        try:
+            self.session.place_active_order(
+                symbol=SYMBOL, side=side, order_type="Market",qty=QTY,
+                time_in_force="GoodTillCancel",reduce_only=False,close_on_trigger=False)
+        except:
+            print("couldn't place order:\n" + traceback.format_exc())
+            
+    def get_last_price(self):
+        try: return  self.session.latest_information_for_symbol(symbol=SYMBOL)['result'][0]['index_price']
+        except: print("couldn't get last price:\n" + traceback.format_exc())
+            
+    def get_entry_price(self, idx):
+        try: return self.session.my_position(symbol=SYMBOL)['result'][idx]['entry_price']
+        except: print("couldn't get entry price:\n" + traceback.format_exc())
+
+    def set_stops(self, sl, tp, side):
+        try: self.session.set_trading_stop(symbol=SYMBOL, side=side, stop_loss=sl, take_profit=tp)
+        except: print("couldn't set stoploss and takeprofit:\n" + traceback.format_exc())
+
 
 
 
