4ca057cd16635e1db5e291c3921c1929d21f58e7
wiktor bozek <wiktorsebbozek@gmail.com>
2023-05-11 00:21:08 +0200

    Check if in trade

--- bot.py
+++ bot.py
@@ -31,36 +31,15 @@ class Bot:
         elif side == "Sell": take_profit = entry_price - entry_price * tp_percent
         return round(take_profit, 2)
 
-    # def in_trade(self):
+    def in_trade(self):
 
-    #     x = self.get_index(self.bybit.session.my_position(symbol=SYMBOL)['result'])
+        x = self.get_index(self.bybit.session.my_position(symbol=SYMBOL)['result'])
 
-    #     # set the entry price
-    #     ep = 0 if x == None else self.bybit.get_entry_price(x)
+        # set the entry price
+        ep = 0 if x == None else self.bybit.get_entry_price(x)
 
-    #     return True if ep != 0 else False
-    
-    # def print_info(self, data):
-    #     macd=Analyser.macd_signal(data)
-    #     psar=Analyser.psar_signal(data)
-    #     ema=Analyser.ema_signal(data)
-    #     lp = self.bybit.get_last_price()
-    #     print(f"Current signals:\nMACD: {macd}; PSAR: {psar}; EMA: {ema}\nLast price of {SYMBOL}: {lp}\n")
+        return True if ep != 0 else False
 
-
-    # def run(self):
-    #     # main sequence
-
-    #     if self.in_trade(): return "Waiting for trade to end"
-
-    #     data = self.bybit.get_format_data()
-    #     signal = Analyser.check_signals(data)
-    #     self.print_info(data)
-    #     if signal != "NO SIGNAL":
-    #         self.bybit.place_order(signal)
-    #         sl = Analyser.psar_price(data)
-    #         self.bybit.set_stops(sl, self.calculate_takeprofit(sl, signal), signal)
-    #         print(f'Position has been opened! {signal} {self.bybit.get_last_price()}')
             
 
     
