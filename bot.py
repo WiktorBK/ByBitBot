bfcd5e3008d6d2485652df59486c3f4909cfb8c9
wiktor bozek <wiktorsebbozek@gmail.com>
2023-05-11 00:19:45 +0200

    Create Bot function

---
+++ bot.py
@@ -0,0 +1,28 @@
+
+import time
+import calendar
+from datetime import datetime
+
+import pandas as pd
+
+from bybit import Bybit
+from config import *
+from analysis import Analyser
+
+class Bot: 
+
+    def __init__(self):
+        self.bybit = Bybit(AKEY, ASECRET)
+        
+
+ 
+            
+
+    
+
+
+
+
+
+
+
