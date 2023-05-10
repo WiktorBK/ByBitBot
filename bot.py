ec9b6a99958409983585b4e7a8a319fcd389ce22
wiktor bozek <wiktorsebbozek@gmail.com>
2023-05-11 00:22:21 +0200

    Print out info

--- bot.py
+++ bot.py
@@ -39,6 +39,15 @@ class Bot:
         ep = 0 if x == None else self.bybit.get_entry_price(x)
 
         return True if ep != 0 else False
+    
+    def print_info(self, data):
+        macd=Analyser.macd_signal(data)
+        psar=Analyser.psar_signal(data)
+        ema=Analyser.ema_signal(data)
+        lp = self.bybit.get_last_price()
+        print(f"Current signals:\nMACD: {macd}; PSAR: {psar}; EMA: {ema}\nLast price of {SYMBOL}: {lp}\n")
+
+
 
             
 
